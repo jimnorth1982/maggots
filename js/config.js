5c3f1dda4dec53f31baa5f8b2fe65811f587c616
Jim North <jimnorth1982@gmail.com>
2020-02-09 20:12:20 -0500

    Adding more structure

--- js/config.js
+++ js/config.js
@@ -1,3 +1,43 @@
-config = {
+class IntroConfig {
+    constructor() {
+        this.game = new Phaser.Game(this.getConfig());
+    }
+
+    getConfig() {
+        return {
+            type: Phaser.AUTO,
+            width: 800,
+            height: 600,
+            physics: {
+                default: 'arcade',
+                arcade: {
+                    gravity: { y: 200 }
+                }
+            },
+            scene: {
+                preload: this.preload,
+                create: this.create,
+                update: this.update
+            }
+        }
+    }
+    
+    getGameObj() {
+        return game;
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
     
 }
+
+var game = new IntroConfig();
